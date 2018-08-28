6108be746f44fce2d0fe7c75b85403a2350eabc1
davi.leitao <dgleitao.contratos@gmail.com>
2018-08-28 08:30:27 -0300

    chapter3/10-growingFunctions

---
+++ chapter3/10-growingFunctions.js
@@ -0,0 +1,14 @@
+// We could find ourselves writing very similar code multiple times.
+// We want to avoid doing that since having more code means more space for mistakes
+// to hide and more material to read for people trying to understand the program.
+// So we take the repetead functionality, find a good name for it, and put it into
+// a function.
+
+// You might even start writing code that uses the function before you actually
+// define the function itself.
+
+// How difficult it is to find a good name for a function is a good indication of
+// how clear a concept it is that you're trying to wrap.
+
+// How start and versatile should our function be? A useful principle is not to add
+// cleverness unless you are absolutely sure you're going to need it.
