cb6491e9282af8b548d0d2a722a9416a313c0f61
Davi Gadelha Leitão <dgleitao.contratos@gmail.com>
2018-08-28 23:35:27 -0300

    chapter4/dataSets

---
+++ chapter4/dataSets.js
@@ -0,0 +1,15 @@
+let listOfNumbers = [2,3,4,5,6,7];
+console.log(listOfNumbers);
+listOfNumbers.pop();
+console.log(listOfNumbers);
+listOfNumbers.push(7);
+console.log(listOfNumbers);
+
+let arrayOfStrings = ['Davi', 'Gadelha', 'Leitão'];
+console.log(arrayOfStrings);
+
+// An array of strings can be flattened to a single string with
+// the join method.
+let linkedArray = arrayOfStrings.join(" ");
+console.log(linkedArray);
+
