5246419c0f96cb3fcb45b4e4d636a793891ac37a
Davi Gadelha Leitão <dgleitao.contratos@gmail.com>
2019-03-08 14:10:53 -0300

    calculating factorials with different types of JavaScript functions notations

---
+++ generalExercises/factorial.js
@@ -0,0 +1,50 @@
+// The goal here is to use the many forms we have to define a function in JavaScript and to
+// calculate the factorial of a number with them.
+
+checkInput = number => {
+  if (number < 0) {
+    return "Negative number.";
+  } else if (number === 0 || number === 1) {
+    return 1;
+  }
+};
+
+// function as values - is just a regular variable definition where the value given to the variable happens to be a function.
+var calculateFactorial = function(number) {
+  checkInput(number);
+
+  var factorial = 1;
+  for (var i = number; i > 1; i--) {
+    factorial = factorial * i;
+  }
+  return factorial;
+};
+
+console.log(calculateFactorial(3));
+
+// declaration notation
+function calculateFactorial2(number) {
+  checkInput(number);
+
+  let factorial = 1;
+  while (number > 1) {
+    factorial = factorial * number;
+    number--;
+  }
+  return factorial;
+}
+
+console.log(calculateFactorial2(3));
+
+calculateFactorial3 = number => {
+  checkInput(number);
+
+  let factorial = 1;
+  while (number > 1) {
+    factorial = factorial * number;
+    number--;
+  }
+  return factorial;
+};
+
+console.log(calculateFactorial3(3));
